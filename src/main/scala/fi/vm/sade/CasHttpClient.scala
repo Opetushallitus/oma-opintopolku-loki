0b931ade0b5fab5fae33a5852bf13b8a1d1c58df
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-09-05 17:04:28 +0300

    extracted casHttpClient

---
+++ src/main/scala/fi/vm/sade/CasHttpClient.scala
@@ -0,0 +1,31 @@
+package fi.vm.sade
+
+import fi.vm.sade.utils.cas.{CasAuthenticatingClient, CasClient, CasParams}
+import org.http4s.client._
+
+
+object CasHttpClient {
+
+  val sessionCookieName = "JSESSIONID"
+
+  def apply(httpClient: Client, scheme_authority: String, params: CasParams = Params.permission) = {
+    CasAuthenticatingClient(
+      new CasClient(scheme_authority, httpClient),
+      params,
+      httpClient,
+      Some(AuditLogParserSubSystemCode.code),
+      sessionCookieName
+    )
+  }
+}
+
+
+object Params {
+  private val permission_path = "/kayttooikeus-service"
+
+  private def username = sys.env("username")
+  private def password = sys.env("password")
+
+  def permission: CasParams = CasParams(permission_path, username, password)
+}
+
--- src/main/scala/fi/vm/sade/RemoteOrganizationRepository.scala
+++ src/main/scala/fi/vm/sade/RemoteOrganizationRepository.scala
@@ -1,6 +1,5 @@
 package fi.vm.sade
 
-import fi.vm.sade.utils.cas.{CasAuthenticatingClient, CasClient, CasParams}
 import org.http4s.{Request}
 import org.http4s.client.{blaze}
 import org.json4s._
@@ -11,8 +10,6 @@ class RemoteOrganizationRepository {
 
   implicit val formats = DefaultFormats
 
-  val sessionCookieName = "JSESSIONID"
-
   val maxHttpRequestThreads = 10
 
   val scheme_authority = "https://virkailija.testiopintopolku.fi"
@@ -21,15 +18,13 @@ class RemoteOrganizationRepository {
   def organizationDetailsURL(oid: String) = s"${scheme_authority}/organisaatio-service/rest/organisaatio/v3/${oid}?includeImage=false"
 
   val blazeHttpClient = blaze.PooledHttp1Client(maxTotalConnections = maxHttpRequestThreads)
-  val casClient = new CasClient(scheme_authority, blazeHttpClient)
-
   val max_api_call_duration = Duration(30, "seconds")
 
   def getOrganizationIdsForUser(oid: String): Array[OrganizationPermission] = {
 
-    val permissionClient = Http(casAuthenticatingClient(Params.permission))
-    val users: Array[User] =
-     permissionClient.get(userOrganizationsURL(oid))(parseResponse[Array[User]]).runFor(max_api_call_duration)
+    val permissionClient = Http(CasHttpClient(blazeHttpClient, scheme_authority))
+    val users: Array[User] = permissionClient.get(userOrganizationsURL(oid))(parseResponse[Array[User]])
+       .runFor(max_api_call_duration)
 
     users.flatMap(user => user.organisaatiot)
   }
@@ -40,18 +35,8 @@ class RemoteOrganizationRepository {
 
     val organizations = getOrganizationIdsForUser(oid)
 
-    organizations.map(org => {
-      organizationClient.get(organizationDetailsURL(org.organisaatioOid))(parseResponse[Organization]).runFor(max_api_call_duration)
-    })
-  }
-
-  private def casAuthenticatingClient(casParams: CasParams) = {
-    CasAuthenticatingClient(
-      casClient,
-      casParams,
-      blazeHttpClient,
-      Some(AuditLogParserSubSystemCode.code),
-      sessionCookieName
+    organizations.map(org => organizationClient.get(organizationDetailsURL(org.organisaatioOid))(parseResponse[Organization])
+      .runFor(max_api_call_duration)
     )
   }
 
@@ -59,24 +44,13 @@ class RemoteOrganizationRepository {
     // TODO: Error handling
     parse(body).extract[T]
   }
-
-  def printResponse(status: Int, body: String, request: Request) = {
-    println(s"Response code: ${status}, body: ${body}")
-  }
 }
 
 object AuditLogParserSubSystemCode {
   val code = "auditlog"
 }
 
-object Params {
-  private val permission_path = "/kayttooikeus-service"
 
-  private def username = sys.env("username")
-  private def password = sys.env("password")
-
-  def permission: CasParams = CasParams(permission_path, username, password)
-}
 
 case class User(oidHenkilo: String, username: String, kayttajaTyyppi: String, organisaatiot: Array[OrganizationPermission])
 case class OrganizationPermission(organisaatioOid: String, kayttooikeudet: Array[Permission])
