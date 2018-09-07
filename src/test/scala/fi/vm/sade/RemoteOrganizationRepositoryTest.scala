388180a67530eca0b083425464f2449b340eacde
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-09-08 01:18:30 +0300

    Test permission JSON parsing

---
+++ src/test/scala/fi/vm/sade/RemoteOrganizationRepositoryTest.scala
@@ -0,0 +1,31 @@
+package fi.vm.sade
+
+import fi.vm.sade.http.HttpClient
+import org.http4s.{Request, Uri}
+import org.scalatest.{FunSpec, Matchers}
+import org.scalamock.scalatest.MockFactory
+import scalaz.concurrent.Task
+
+import scala.io.Source
+
+
+class RemoteOrganizationRepositoryTest extends FunSpec with Matchers with MockFactory {
+  describe("A RemoteOrganizationRepository") {
+
+    it("Should be able to get organizations for user") {
+
+      val repository = new RemoteOrganizationRepository {
+        override protected lazy val casHttpClient: HttpClient = new HttpClient {
+          override def get[ResultType](uri: Uri)(decode: Decode[ResultType]): Task[ResultType] = {
+            Task.now[ResultType](decode(200, Source.fromResource("permissions.json").mkString, null))
+          }
+        }
+      }
+
+      val permissions: Array[OrganizationPermission] = repository.getOrganizationIdsForUser("1.2.3")
+
+      assume(permissions.length == 1, "Contains one permission")
+      assume(permissions.headOption.get.organisaatioOid == "1.2.246.562.10.82388989657", "Has correct organization oid")
+    }
+  }
+}
