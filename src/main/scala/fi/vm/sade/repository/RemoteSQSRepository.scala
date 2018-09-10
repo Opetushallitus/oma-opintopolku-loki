601e5484d0ca3aa9b72b9daa6072bdb0f168f317
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-09-10 14:23:59 +0300

    Get 10 messages at a time, helper function to approximate if queue is empty

--- src/main/scala/fi/vm/sade/repository/RemoteSQSRepository.scala
+++ src/main/scala/fi/vm/sade/repository/RemoteSQSRepository.scala
@@ -4,22 +4,35 @@ import java.util
 
 import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder
-import com.amazonaws.services.sqs.model.{DeleteMessageRequest, DeleteMessageResult, Message}
+import com.amazonaws.services.sqs.{AmazonSQS, AmazonSQSClientBuilder}
+import com.amazonaws.services.sqs.model._
 import fi.vm.sade.conf.Configuration._
+import scala.collection.JavaConverters._
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest
 
 object RemoteSQSRepository {
 
   private def endpointConfiguration: EndpointConfiguration = new AwsClientBuilder.EndpointConfiguration(SQSHost, awsRegion)
 
-  private val sqs = AmazonSQSClientBuilder.standard().withEndpointConfiguration(endpointConfiguration).build()
+  private val sqs: AmazonSQS = AmazonSQSClientBuilder.standard().withEndpointConfiguration(endpointConfiguration).build()
   private val queueUrl: String = sqs.getQueueUrl(SQSQueueName).getQueueUrl
 
+  private val approximateNumberOfMessages = QueueAttributeName.ApproximateNumberOfMessages.toString
+
   def getMessages: util.List[Message] = {
-    sqs.receiveMessage(queueUrl).getMessages
+    sqs.receiveMessage(new ReceiveMessageRequest(queueUrl).withMaxNumberOfMessages(10)).getMessages
   }
 
   def deleteMessage(receiptHandle: String): DeleteMessageResult = {
     sqs.deleteMessage(new DeleteMessageRequest(queueUrl, receiptHandle))
+  }
+
+  def hasMessages: Boolean = {
+    sqs.getQueueAttributes(queueUrl, List(approximateNumberOfMessages).asJava)
+      .getAttributes.get(approximateNumberOfMessages).toInt > 0
+  }
+
+  private def sendMessage(message: String) = {
+    sqs.sendMessage(new SendMessageRequest(queueUrl, message))
   }
 }
