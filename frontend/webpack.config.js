b281ded3c1bf97b37bae35f6c8035ac470b22318
Petri Myllys <petri.myllys@reaktor.com>
2018-09-13 09:21:14 +0300

    Do lint with loader

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -7,7 +7,14 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' }
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'eslint-loader'
+        ]
+      }
     ]
   },
   devServer: {
