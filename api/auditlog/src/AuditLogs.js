337d158679eae20c06d44b4a833f88c479c048f2
suarela <aleksi.saarela@reaktor.fi>
2018-09-19 14:11:40 +0300

    quering logs by oid

---
+++ api/auditlog/src/AuditLogs.js
@@ -0,0 +1,42 @@
+class AuditLogs {
+  constructor(db) {
+    this.db = db
+  }
+
+  _groupByOrganization(array) {
+    return array
+      .map(({ time, organizationOid }) => organizationOid.map(orgOid => ({ time, orgOid })))
+      .reduce((array, val) => array.concat(val), [])
+      .reduce((obj, { time, orgOid }) => (
+        obj[orgOid]
+          ? { ...obj, [orgOid]: obj[orgOid].concat(time) }
+          : { ...obj, [orgOid]: [time] }
+      ),
+        {}
+      )
+  }
+
+  getAllForOid(oid) {
+    const params = {
+      TableName: "LogEntry",
+      KeyConditionExpression: "studentOid = :sOid",
+      ExpressionAttributeValues: {
+        ":sOid": oid
+      }
+    }
+
+    return new Promise(
+      (resolve, reject) => {
+        this.db.query(params, (error, data) => {
+          if (error) reject(error)
+
+          const { Items } = data
+          const grouped = this._groupByOrganization(Items)
+          resolve(grouped)
+        })
+      }
+    )
+  }
+}
+
+module.exports = AuditLogs
