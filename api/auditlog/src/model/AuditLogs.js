ff5e7f09880a4b27f985265576fb6d21b26b7bb2
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-09-24 15:33:47 +0300

    Return organization names if applicable

--- api/auditlog/src/model/AuditLogs.js
+++ api/auditlog/src/model/AuditLogs.js
@@ -1,14 +1,22 @@
+const axios = require('axios');
+
 class AuditLogs {
   constructor(db) {
     this.db = db
+
+    this.http = axios.create({
+      baseURL: 'https://dev.koski.opintopolku.fi',
+      timeout: 5000
+    });
   }
 
   _objectToArray(obj) {
-    return Object.keys(obj)
-      .map(key => ({
+    return Promise.all(Object.keys(obj)
+      .map(async key => ({
         organizationOid: key,
+        organizationNames: await this._getOrganizationNames(key),
         timestamps: obj[key]
-      }))
+      })))
   }
 
   _groupByOrganization(array) {
@@ -24,6 +32,22 @@ class AuditLogs {
       )
   }
 
+  async _getOrganizationNames(oid) {
+    try {
+      if (oid === null || typeof oid === 'undefined' || oid === "self") return null
+      console.log(JSON.stringify({message: `Getting organization name for ${oid}`}))
+
+      const response = await this.http.get(`/organisaatio-service/rest/organisaatio/v3/${oid}`)
+      return response.data.nimi
+    } catch (e) {
+      console.log(JSON.stringify({
+        error: e.message,
+        message: 'Failed to get organization names'
+      }))
+      return null
+    }
+  }
+
   getAllForOid(oid) {
     const params = {
       TableName: "AuditLog",
@@ -35,13 +59,13 @@ class AuditLogs {
 
     return new Promise(
       (resolve, reject) => {
-        this.db.query(params, (error, data) => {
+        this.db.query(params, async (error, data) => {
           if (error) return reject(error)
           if (data === null || typeof data === 'undefined') return resolve([])
 
           const { Items } = data
           const grouped = this._groupByOrganization(Items)
-          const asArray = this._objectToArray(grouped)
+          const asArray = await this._objectToArray(grouped)
           resolve(asArray)
         })
       }
