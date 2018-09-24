f55466cff55dc27568bfa22cb3c8a74cb9f79b68
Petri Myllys <petri.myllys@reaktor.com>
2018-09-24 17:56:12 +0300

    Improve translation function

--- frontend/src/util/translate.js
+++ frontend/src/util/translate.js
@@ -5,17 +5,24 @@ import { lang } from 'util/preferences'
  * Expects the translation lookup map to exist as a global (window) variable.
  * Prefer using this via the tagged template notation.
  * @example <pre><code>t`Oma Opintopolku`</code></pre>
- * @param {string} key Term to look up from the translation map
+ * @param {string} key Term (non-interpolated string when using tagged template) to look up from the translation map
  * @returns {string} Translated text if translation found, else key
  */
 export default key => {
+  const isTaggedTemplate = key.hasOwnProperty('raw')
+
+  if (isTaggedTemplate && key.length > 1) {
+    console.error('Unsupported translation usage: Using tagged template notation does not support interpolation.')
+  }
+
+  const parsedKey = isTaggedTemplate ? key[0] : key
   const translations = window.translationsMap || {}
-  const versions = translations[key] || {}
+  const versions = translations[parsedKey] || {}
   const translated = versions[lang]
 
   if (!translated) {
-    console.error(`Translation missing for language ${lang}: ${key}`)
-    return key
+    console.error(`Translation missing for language ${lang}: ${parsedKey}`)
+    return parsedKey
   }
 
   return translated
