54808947894a81baa6514ce2409fb6a45735df77
Petri Myllys <petri.myllys@reaktor.com>
2018-09-24 15:49:33 +0300

    Fetch origanization names with logs

--- frontend/src/component/LogEntries.jsx
+++ frontend/src/component/LogEntries.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { uniq } from 'ramda'
+import { lensProp, omit, over, uniq } from 'ramda'
 import Query from 'http/Query'
 import LogEntry from 'component/LogEntry'
 import { lang } from 'util/preferences'
 
 const getTranslatedName = organizationName => organizationName[lang] || ''
+const nameLens = lensProp('organizationName')
 
 const LogEntries = () => (
   <Query url='logs'>
@@ -12,22 +13,12 @@ const LogEntries = () => (
       if (error) return <div>{'Oops!'}</div>
       if (pending) return <div>{'Wait!'}</div>
 
-      return (
-        <Query
-          url={`${process.env.ORGANISAATIO_SERVICE_BASE_URL}/organisaatio/v4/findbyoids`}
-          method='post'
-          body={uniq(data.map(v => v.organizationOid))}
-        >
-          {({ data, error, pending }) => {
-            if (error) return <div>{'Oops!'}</div>
-            if (pending) return <div>{'Wait!'}</div>
+      const organizations = uniq(data
+        .map(omit(['timestamp']))
+        .map(over(nameLens, getTranslatedName)))
 
-            return data.map(({ nimi }) => {
-              const name = getTranslatedName(nimi)
-              return <LogEntry key={name} organizationName={name} />
-            })
-          }}
-        </Query>
+      return organizations.map(({ organizationOid, organizationName }) =>
+        <LogEntry key={organizationOid} organizationName={organizationName} />
       )
     }}
   </Query>
