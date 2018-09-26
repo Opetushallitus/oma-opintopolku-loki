7705754d06b2a76eab248ccbcd9e1c4ed55d3c63
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-09-26 14:52:49 +0300

    var changed to const, functions changed to arrow functions

--- api/cas-client/cas.js
+++ api/cas-client/cas.js
@@ -1,18 +1,18 @@
-var querystring = require('querystring');
-var https = require('https');
-var format = require('string-format');
+const querystring = require('querystring');
+const https = require('https');
+const format = require('string-format');
 
 format.extend(String.prototype);
 
 const clientSubSystemCode = 'oma-opintopolku-loki-api';
 
-function getTgt(username, password, hostname, callback) {
-  var credentials = querystring.stringify({
+getTgt = (username, password, hostname, callback) => {
+  const credentials = querystring.stringify({
     username: username,
     password: password
   });
 
-  var req = https.request({
+  const req = https.request({
     hostname: hostname,
     port: 443,
     path: '/cas/v1/tickets',
@@ -22,17 +22,14 @@ function getTgt(username, password, hostname, callback) {
       'Content-Length': credentials.length,
       clientSubSystemCode
     }
-  }, function(res) {
+  }, (res) => {
     if (res.statusCode === 201)
       callback(res.headers.location);
-    else {
-      console.log(res)
+    else
       throw new Error('TGT:n hakeminen ei onnistunut, status code: {0}'.format(res.statusCode));
-    }
-
   });
 
-  req.on('error', function(e) {
+  req.on('error', (e) => {
     console.log('TGT virhe: ' + e.message);
     throw e;
   });
@@ -41,12 +38,12 @@ function getTgt(username, password, hostname, callback) {
   req.end();
 }
 
-function getSt(hostname, tgtUrl, service, callback) {
-  var serviceUrl = querystring.stringify({
+getSt = (hostname, tgtUrl, service, callback) => {
+  const serviceUrl = querystring.stringify({
     service: service
   });
 
-  var req = https.request({
+  const req = https.request({
     hostname: hostname,
     port: 443,
     path: tgtUrl,
@@ -56,16 +53,16 @@ function getSt(hostname, tgtUrl, service, callback) {
       'Content-Length': serviceUrl.length,
       clientSubSystemCode
     }
-  }, function(res) {
+  }, (res) => {
     if (res.statusCode === 200)
-      res.on('data', function (body) {
+      res.on('data', (body) => {
         callback('' + body)
       });
     else
       throw new Error('ST:n hakeminen ei onnistunut, status code: {0}'.format(res.statusCode))
   });
 
-  req.on('error', function(e) {
+  req.on('error', (e) => {
     console.log('ST virhe: ' + e.message);
     throw e;
   });
@@ -74,10 +71,10 @@ function getSt(hostname, tgtUrl, service, callback) {
   req.end();
 }
 
-function getCookie(hostname, service, st, callback) {
-  https.get('https://{0}/{1}/?ticket={2}'.format(hostname, service, st), function(res) {
+getCookie = (hostname, service, st, callback) => {
+  https.get('https://{0}/{1}/?ticket={2}'.format(hostname, service, st), (res) => {
     if (res.statusCode === 200) {
-      var jsessionId = res.headers['set-cookie'].filter(function(header) {
+      const jsessionId = res.headers['set-cookie'].filter((header) => {
         return header.match(/^JSESSIONID=.*$/)
       });
       if (jsessionId.length > 0)
@@ -86,17 +83,17 @@ function getCookie(hostname, service, st, callback) {
         throw new Error("JSESSIONID cookieta ei löytynyt")
     }
     else callback(null);
-  }).on('error', function(e) {
+  }).on('error', (e) => {
     console.error(e);
     callback(null);
   });
 }
 
 module.exports = {
-  withCookie: function(options, cookieCallback) {
-    getTgt(options.username, options.password, options.hostname, function(tgtUrl) {
-      getSt(options.hostname, tgtUrl, 'https://{0}/{1}/j_spring_cas_security_check'.format(options.hostname, options.service), function(st) {
-        getCookie(options.hostname, (options.cookieUrl ? options.cookieUrl : options.service), st, function(cookie) {
+  withCookie: (options, cookieCallback) => {
+    getTgt(options.username, options.password, options.hostname, (tgtUrl) => {
+      getSt(options.hostname, tgtUrl, 'https://{0}/{1}/j_spring_cas_security_check'.format(options.hostname, options.service), (st) => {
+        getCookie(options.hostname, (options.cookieUrl ? options.cookieUrl : options.service), st, (cookie) => {
           cookieCallback(cookie)
         })
       })
