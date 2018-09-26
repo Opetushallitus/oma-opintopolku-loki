efe219c0fb0444d75d8aec996e58eb99200a1733
suarela <aleksi.saarela@reaktor.fi>
2018-09-26 16:44:46 +0300

    groups timestamp based on array of org oids

--- api/auditlog/src/model/AuditLogs.js
+++ api/auditlog/src/model/AuditLogs.js
@@ -8,43 +8,66 @@ class AuditLogs {
 
     this.http = axios.create({
       baseURL: config.get('backend.url'),
-      timeout: config.get('backend.timeout'),
-    });
-  }
-
-  _toOrganizationArray(obj) {
-    return Promise.all(Object.keys(obj)
-      .map(async key => ({
-        organizationOid: key,
-        organizationNames: await this._getOrganizationNames(key),
-        timestamps: obj[key]
-      })))
-  }
-
-  _groupByOrganization(array) {
-    return array
-      .map(({ time, organizationOid }) => organizationOid.map(orgOid => ({ time, orgOid })))
-      .reduce((array, val) => array.concat(val), [])
-      .reduce((obj, { time, orgOid }) => (
-        obj[orgOid]
-          ? { ...obj, [orgOid]: obj[orgOid].concat(time) }
-          : { ...obj, [orgOid]: [time] }
-      ),
-        {}
-      )
+      timeout: config.get('backend.timeout')
+    })
   }
 
   async _getOrganizationNames(oid) {
     try {
-      if (oid === null || typeof oid === 'undefined' || oid === "self") return null
+      if (oid === null || typeof oid === 'undefined' || oid === "self") return { oid, name: null }
       log.info(`Getting organization name for ${oid}`)
 
       const response = await this.http.get(`/organisaatio-service/rest/organisaatio/v3/${oid}`)
-      return response.data.nimi
+      const { name: nimi } = response.data
+      return {
+        name,
+        oid
+      }
     } catch (error) {
       log.error(`Failed to get organization names for ${oid}`, { error })
       return null
     }
+  }
+
+  async _getOrganizationNameMap(oids) {
+    const orgNames = await Promise.all(oids.map(async oid => await this._getOrganizationNames(oid)))
+
+    return orgNames.reduce((obj, val) => ({ ...obj, [val.oid]: val.name }), {})
+  }
+
+  _getOrganizationOids(logsGroupedByOrgs) {
+    return Object.keys(logsGroupedByOrgs)
+      .map(str => str.split(','))
+      .reduce((array, organizationOids) => array.concat(organizationOids), [])
+      .filter((val, i, self) => self.indexOf(val) === i)
+  }
+
+  _mapNamesToOrganizationOids(logsGroupedByOrgs, nameMap) {
+    return Object.keys(logsGroupedByOrgs)
+      .map(str => str.split(','))
+      .map(key => ({
+        organizations: key.map(oid => ({ oid, name: nameMap[oid] })),
+        timestamps: logsGroupedByOrgs[key]
+      }))
+  }
+
+  async _mapOrganizationNames(logsGroupedByOrgs) {
+    const uniqueOrgOids = this._getOrganizationOids(logsGroupedByOrgs)
+    const nameMap = await this._getOrganizationNameMap(uniqueOrgOids)
+
+    return this._mapNamesToOrganizationOids(logsGroupedByOrgs, nameMap)
+  }
+
+  _groupByOrganizationOids(auditlogs) {
+    return auditlogs
+      .map(({ time, organizationOid }) => ({ time, orgOids: organizationOid.sort() }))
+      .reduce((obj, { time, orgOids }) => (
+        obj[orgOids]
+          ? { ...obj, [orgOids]: obj[orgOids].concat(time) }
+          : { ...obj, [orgOids]: [time] }
+      ),
+        {}
+      )
   }
 
   getAllForOid(oid) {
@@ -60,11 +83,11 @@ class AuditLogs {
       (resolve, reject) => {
         this.db.query(params, async (error, data) => {
           if (error) return reject(error)
-          if (data === null || typeof data === 'undefined') return resolve([])
+          if (data === null || typeof data === 'undefined') return resolve([])
 
           const { Items } = data
-          const grouped = this._groupByOrganization(Items)
-          const asArray = await this._toOrganizationArray(grouped)
+          const grouped = this._groupByOrganizationOids(Items)
+          const asArray = await this._mapOrganizationNames(grouped)
           resolve(asArray)
         })
       }
