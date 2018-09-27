4a42a83336701ce76ec5c8f1dd8fa7d31c70bd62
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-09-27 11:19:53 +0300

    Use UserClient for whoami

--- api/whoami/src/entrypoint.js
+++ api/whoami/src/entrypoint.js
@@ -1,4 +1,17 @@
-module.exports.handler = (event, context, callback) => {
+const UserClient = require('../../common/client')
+
+module.exports.handler = async (event, context, callback) => {
+
+  try {
+    const jooh = await UserClient({
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+    }, '070598-6593')
+
+    console.log(jooh.data)
+  } catch (err) {
+    console.log('Failed', err)
+  }
 
   const { firstname, sn: lastname, oid } = event.headers
   console.log(JSON.stringify({message: `Received whoami request for ${oid}`}))
