6319c1ae76b97387acb09771c83ad68ed242d819
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-09-27 13:51:36 +0300

    Verify shibboleth secret before doing hetu lookup

--- api/whoami/src/entrypoint.js
+++ api/whoami/src/entrypoint.js
@@ -1,5 +1,10 @@
+const AWS = require('aws-sdk')
 const config = require('config');
+
+const SecretManager = require('../../common/auth/SecretManager')
 const UserClient = require('../../common/UserClient')
+
+const secretManager = new SecretManager(new AWS.SecretsManager(), config.get('secret.name'))
 
 module.exports.handler = async (event, context, callback) => {
 
@@ -7,21 +12,33 @@ module.exports.handler = async (event, context, callback) => {
     const { secret, hetu, oid } = event.headers
     console.log(JSON.stringify({message: `Received whoami request for ${oid}`}))
 
-    const userClient = new UserClient(
-      config.get('backend.timeout'),
-      config.get('backend.host'),
-      config.get('secret.name')
-    )
+    const isAuthenticated = await secretManager.authenticateRequest(secret)
 
-    const user = await userClient.getUser(hetu)
+    if (isAuthenticated) {
 
-    callback(null, {
-      statusCode: 200,
-      headers: {
-        'Cache-Control': 'max-age=600'
-      },
-      body: JSON.stringify(user)
-    })
+      const userClient = new UserClient(
+        config.get('backend.timeout'),
+        config.get('backend.host'),
+        config.get('secret.name')
+      )
+
+      const user = await userClient.getUser(hetu)
+
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          'Cache-Control': 'max-age=600'
+        },
+        body: JSON.stringify(user)
+      })
+    } else {
+
+      callback(null, {
+        statusCode: 401,
+        body: JSON.stringify({message: 'Not authenticated'})
+      })
+
+    }
   } catch (err) {
     console.log('Failed', err)
   }
