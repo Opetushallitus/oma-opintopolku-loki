ae8988b69ced872677acc42e3826b2a0fbf13243
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-09-27 13:34:44 +0300

    whoami returns user gotten from API

--- api/whoami/src/entrypoint.js
+++ api/whoami/src/entrypoint.js
@@ -1,30 +1,28 @@
-const UserClient = require('../../common/client')
+const config = require('config');
+const UserClient = require('../../common/UserClient')
 
 module.exports.handler = async (event, context, callback) => {
 
   try {
-    const jooh = await UserClient({
-      username: process.env.USERNAME,
-      password: process.env.PASSWORD,
-    }, '070598-6593')
+    const { secret, hetu, oid } = event.headers
+    console.log(JSON.stringify({message: `Received whoami request for ${oid}`}))
 
-    console.log(jooh.data)
+    const userClient = new UserClient(
+      config.get('backend.timeout'),
+      config.get('backend.host'),
+      config.get('secret.name')
+    )
+
+    const user = await userClient.getUser(hetu)
+
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        'Cache-Control': 'max-age=600'
+      },
+      body: JSON.stringify(user)
+    })
   } catch (err) {
     console.log('Failed', err)
   }
-
-  const { firstname, sn: lastname, oid } = event.headers
-  console.log(JSON.stringify({message: `Received whoami request for ${oid}`}))
-
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      'Cache-Control': 'max-age=600'
-    },
-    body: JSON.stringify({
-      firstname,
-      lastname,
-      oid
-    })
-  })
 }
