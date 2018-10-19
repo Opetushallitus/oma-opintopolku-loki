f32f150261fa3cb8a97e11095c68ac31309452a1
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-10-19 10:17:13 +0300

    Parse birthday from hetu

--- frontend/src/component/generic/widget/StudentInfo.jsx
+++ frontend/src/component/generic/widget/StudentInfo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { H3 } from 'ui/typography'
 import http from 'http/http'
-import { parseUserName } from '../../../service/raamitSupport'
+import { parseUserName } from 'service/raamitSupport'
 
 const Student = styled(H3)`
   margin-top: 3rem;
@@ -17,7 +17,7 @@ const Birthday = styled.span`
 `
 
 class StudentInfo extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
 
     this.state = {
@@ -26,25 +26,57 @@ class StudentInfo extends React.Component {
     }
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.getStudentDetails()
   }
 
-  async getStudentDetails() {
+  parseCentury (hetu) {
+    const pattern = /\d{6}([-A+])/
+    const matches = hetu.match(pattern)
+    const separator = matches[1]
+
+    switch (separator) {
+      case '+': return 1800
+      case '-': return 1900
+      case 'A': return 2000
+    }
+  }
+
+  parseBirthday (hetu) {
+    try {
+      const pattern = /(\d{2})/g
+      const matches = hetu.match(pattern)
+
+      const day = parseInt(matches[0])
+      const month = parseInt(matches[1])
+      const yearSuffix = parseInt(matches[2])
+
+      const year = this.parseCentury(hetu) + yearSuffix
+
+      return `${day}.${month}.${year}`
+    } catch (err) {
+      // TODO: Log errors remotely
+    }
+  }
+
+  async getStudentDetails () {
     const whoami = await http.get('whoami')
     this.setState({
       name: parseUserName(whoami.data),
-      birthday: whoami.data.syntymaaika
+      birthday: this.parseBirthday(whoami.data.hetu)
     })
   }
 
-  render() {
-    if (!this.state.name || !this.state.birthday) return null
+  render () {
+    if (!this.state.name) return null
 
     return (
       <Student>
         <Name>{this.state.name}</Name>
-        <Birthday> s. {this.state.birthday}</Birthday>
+        { this.state.birthday
+          ? <Birthday> s. {this.state.birthday}</Birthday>
+          : null
+        }
       </Student>
     )
   }
