3ece43ec29f1eb98ca4274a5b444caf3844e06e2
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-10-19 16:00:51 +0300

    Display notification text if no audit log entries found for user

--- frontend/src/component/Log.jsx
+++ frontend/src/component/Log.jsx
@@ -3,12 +3,20 @@ import { lensProp, map, over, view } from 'ramda'
 import Query from 'http/Query'
 import { lang } from 'util/preferences'
 import { AlertText } from 'ui/typography'
+import constants from 'ui/constants'
 import t from 'util/translate'
 import Organization from 'component/organization/Organization'
+import styled from 'styled-components'
 
 const organizationLens = lensProp('organizations')
 const nameLens = lensProp('name')
 const oidLens = lensProp('oid')
+
+const NotificationText = styled.div`
+  margin: 4.429rem 0;
+  font-size: ${constants.font.size.xl};
+  letter-spacing: 0.0214rem;
+`
 
 const getTranslatedName = organizationName => organizationName[lang] || ''
 
@@ -17,6 +25,7 @@ const Log = () => (
     {({ data, error, pending }) => {
       if (error) return <AlertText>{t`Tapahtui odottamaton virhe, emmekä juuri nyt pysty näyttämään tietoja.`}</AlertText>
       if (pending) return <div>{t`Tietoja haetaan`}</div>
+      if (Array.isArray(data) || !data) return <NotificationText>{t`Sivu on tyhjä, koska tietojasi ei ole vielä käytetty`}</NotificationText>
 
       const translatedOrganizations = map(over(organizationLens, map(over(nameLens, getTranslatedName))))(data)
 
