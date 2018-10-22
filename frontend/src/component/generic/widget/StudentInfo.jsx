4efb2f50a2672ba7ab335be97cb76a85c9552291
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-10-22 10:57:08 +0300

    Use Query component instead of request during componentDidMount

--- frontend/src/component/generic/widget/StudentInfo.jsx
+++ frontend/src/component/generic/widget/StudentInfo.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import { H3 } from 'ui/typography'
-import http from 'http/http'
 import { parseUserName } from 'service/raamitSupport'
+import Query from 'http/Query'
 
 const Student = styled(H3)`
   margin-top: 3rem;
@@ -16,70 +16,54 @@ const Birthday = styled.span`
   font-weight: 400;
 `
 
-class StudentInfo extends React.Component {
-  constructor (props) {
-    super(props)
+const parseCentury = (hetu) => {
+  const pattern = /\d{6}([-A+])/
+  const matches = hetu.match(pattern)
+  const separator = matches[1]
 
-    this.state = {
-      name: null,
-      birthday: null
-    }
-  }
-
-  componentDidMount () {
-    this.getStudentDetails()
-  }
-
-  parseCentury (hetu) {
-    const pattern = /\d{6}([-A+])/
-    const matches = hetu.match(pattern)
-    const separator = matches[1]
-
-    switch (separator) {
-      case '+': return 1800
-      case '-': return 1900
-      case 'A': return 2000
-    }
-  }
-
-  parseBirthday (hetu) {
-    try {
-      const pattern = /(\d{2})/g
-      const matches = hetu.match(pattern)
-
-      const day = parseInt(matches[0])
-      const month = parseInt(matches[1])
-      const yearSuffix = parseInt(matches[2])
-
-      const year = this.parseCentury(hetu) + yearSuffix
-
-      return `${day}.${month}.${year}`
-    } catch (err) {
-      // TODO: Log errors remotely
-    }
-  }
-
-  async getStudentDetails () {
-    const whoami = await http.get('whoami')
-    this.setState({
-      name: parseUserName(whoami.data),
-      birthday: this.parseBirthday(whoami.data.hetu)
-    })
-  }
-
-  render () {
-    if (!this.state.name) return null
-
-    return (
-      <Student>
-        <Name>{this.state.name}</Name>
-        { this.state.birthday
-          ? <Birthday> s. {this.state.birthday}</Birthday>
-          : null
-        }
-      </Student>
-    )
+  switch (separator) {
+    case '+':
+      return 1800
+    case '-':
+      return 1900
+    case 'A':
+      return 2000
   }
 }
+
+const parseBirthday = (hetu) => {
+  try {
+    const pattern = /(\d{2})/g
+    const matches = hetu.match(pattern)
+
+    const day = parseInt(matches[0])
+    const month = parseInt(matches[1])
+    const yearSuffix = parseInt(matches[2])
+
+    const year = parseCentury(hetu) + yearSuffix
+
+    return `${day}.${month}.${year}`
+  } catch (err) {
+    console.log('Failed to parse birthday', err) // TODO: Log errors remotely
+  }
+}
+
+const StudentInfo = () => (
+  <Query url='whoami'>
+    {({ data, error, pending }) => {
+      if (error || pending) return null
+
+      const name = parseUserName(data)
+      const birthday = parseBirthday(data.hetu)
+
+      return (
+        <Student>
+          <Name>{name}</Name>
+          { birthday ? <Birthday> s. {birthday}</Birthday> : null }
+        </Student>
+      )
+    }}
+  </Query>
+)
 
 export default StudentInfo
