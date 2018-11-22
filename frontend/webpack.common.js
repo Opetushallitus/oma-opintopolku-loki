2cc1e7e6f007fd3464bbdc7fb4e95cef81a185c7
Tommi Haapaniemi <tommih@gmail.com>
2018-11-22 13:53:36 +0200

    Generate index.html with webpack

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   resolve: {
@@ -40,4 +41,15 @@ module.exports = {
       }
     ]
   },
+  entry: "./src/index.js",
+  output: {
+      filename: "main.js"
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          filename: 'index.html',
+          template: './src/index.html',
+          title: 'Oma opintopolku - tietojeni käyttö'
+      })
+  ]
 }
