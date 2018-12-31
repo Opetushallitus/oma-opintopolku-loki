b5a3e7069175beefa09687b9996f023369b614f9
suarela <aleksi.saarela@reaktor.fi>
2018-12-31 13:35:46 +0200

    lowers coverage threshold

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -15,10 +15,10 @@ module.exports = {
   snapshotSerializers: ['enzyme-to-json/serializer'],
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95
     }
   },
   preset: 'jest-puppeteer',
