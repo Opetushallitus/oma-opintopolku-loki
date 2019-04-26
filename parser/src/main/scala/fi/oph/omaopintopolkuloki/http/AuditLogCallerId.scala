06090cbe0a27ad7bb039b1f72a5fa283a8215e90
Henry Heikkinen <henry.heikkinen@reaktor.com>
2019-04-26 16:18:46 +0300

    Remove character '/' from parser Caller-Id

--- parser/src/main/scala/fi/oph/omaopintopolkuloki/http/AuditLogCallerId.scala
+++ parser/src/main/scala/fi/oph/omaopintopolkuloki/http/AuditLogCallerId.scala
@@ -1,5 +1,5 @@
 package fi.oph.omaopintopolkuloki.http
 
 object AuditLogCallerId {
-  val code = "1.2.246.562.10.00000000001.oma-opintopolku-loki/parser"
+  val code = "1.2.246.562.10.00000000001.oma-opintopolku-loki-parser"
 }
