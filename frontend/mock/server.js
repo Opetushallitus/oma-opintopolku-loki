bbfa38bee6134b2591e2007b3538bea2990586cd
Henri Ruoho <henri.ruoho@reaktor.com>
2023-01-05 13:07:45 +0200

    Lisää middleware mock-serveriin
    Tarpeelliset muutokset jotta mock-api osaa vastata POST-kyselyyn oikein

---
+++ frontend/mock/server.js
@@ -0,0 +1,29 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('mock/api.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+server.use(jsonServer.bodyParser)
+
+server.use(transformPostRequestToGet)
+server.use(jsonServer.rewriter({
+  "/auditlogs/010280-952L": "/auditlogs",
+  "/auditlogs/010290-911K": "/auditlogs-huollettava"
+}))
+
+server.use(router)
+
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
+
+function transformPostRequestToGet(req, res, next) {
+  if (req.method === 'POST') {
+    const hetu = req.body?.hetu || ''
+    req.method = 'GET'
+    req.url = req.url + hetu
+  }
+  next()
+}
