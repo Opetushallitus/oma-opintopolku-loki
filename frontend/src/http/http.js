3910be5021c9e46b4e0f6a2d3feab3fef5822bf6
Henri Ruoho <henri.ruoho@reaktor.com>
2023-01-10 15:03:27 +0200

    Lisää post-kutsun tarvitsemat headerit

--- frontend/src/http/http.js
+++ frontend/src/http/http.js
@@ -1,20 +1,28 @@
 import axios from 'axios'
+import Cookies from 'js-cookie'
 
 const client = axios.create({
   baseURL: process.env.API_BASE_URL
 })
 
-const get = async url => client.get(url)
-const post = async (url, data) => client.post(url, data)
+const callerId = '1.2.246.562.10.00000000001.oma-opintopolku-loki.frontend'
 
-const request = async (url, method, data = {}) => {
+const get = async url => client.get(url)
+const post = async (url, data, config) => client.post(url, data, config)
+
+const request = async (url, method, data = {}, config = {
+  headers: {
+    'Caller-Id': callerId,
+    CSRF: Cookies.get('CSRF')
+  }
+}) => {
   switch (method) {
     case 'GET':
     case 'get':
       return get(url)
     case 'POST':
     case 'post':
-      return post(url, data)
+      return post(url, data, config)
     default:
       throw new Error(`Unsupported HTTP method: ${method}`)
   }
--- frontend/src/http/http.test.js
+++ frontend/src/http/http.test.js
@@ -12,6 +12,7 @@ describe('http', () => {
   const url = 'http://localhost'
   const responseData = { status: 'ok' }
   const requestData = { foo: 'bar' }
+  const headers = { headers: { CSRF: undefined, 'Caller-Id': '1.2.246.562.10.00000000001.oma-opintopolku-loki.frontend' } }
 
   it('supports method get', () => {
     return http.request(url, 'get').then(response => {
@@ -30,14 +31,14 @@ describe('http', () => {
   it('supports method post', () => {
     return http.request(url, 'post', requestData).then(response => {
       expect(response).toEqual(responseData)
-      expect(mockAxios.post).toHaveBeenCalledWith(url, requestData)
+      expect(mockAxios.post).toHaveBeenCalledWith(url, requestData, headers)
     })
   })
 
   it('supports method POST', () => {
     return http.request(url, 'POST', requestData).then(response => {
       expect(response).toEqual(responseData)
-      expect(mockAxios.post).toHaveBeenCalledWith(url, requestData)
+      expect(mockAxios.post).toHaveBeenCalledWith(url, requestData, headers)
     })
   })
 
